b7f8c37d3076e97dca4394201e34e7c96ad7de29
mykhailo.mikus <mykhailo.mikus@eleks.com>
2017-09-14 10:55:37 +0300

    build bat

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@ setx M2_HOME "%SourcesPath%\apache-maven-3.5.0"
 setx M2 "%SourcesPath%\apache-maven-3.5.0"
 setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_91"
 setx PATH "%M2_HOME%\bin\;%JAVA_HOME%\bin\"
-start cmd mvn -v
-start cmd mvn install
+set
+mvn -v
+mvn install
